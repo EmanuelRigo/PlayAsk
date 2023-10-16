748cc102013d5dbb5ef070b82bc2aed6acd2304a
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2023-10-16 19:58:24 -0300

    terminando logica // pc escritorio

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Question } from "../components/Question";
-import { questions } from "../data";
+import { questions, imgs } from "../data";
 
 const shuffleArray = array => {
     const newArray = array.sort(() => Math.random() - 0.5);
@@ -12,13 +12,16 @@ export const CategoryPage = () => {
 
     const { category } = useParams()
 
+    const [imgCategory] = imgs.filter(
+		img => img === `/src/assets/${category.toLowerCase()}.png`
+	);
 
     const [questionsFiltered, setQuestionsFiltered] = useState(
-        questions.filter(question => question.category === category)
-
-    );
+		questions.filter(question => question.category === category)
+	);
 
     const [indexQuestion, setIndexQuestion] = useState(0)
+    const [activeQuiz, setActiveQuiz] = useState(false)
 
     useEffect(() => {
         const newQuestions = shuffleArray(questionsFiltered);
@@ -30,11 +33,39 @@ export const CategoryPage = () => {
         <div className="container flex flex-col items-center justify-center gap-10"
             style={{ height: ' calc(100vh - 5rem' }} >
 
+            {activeQuiz ? (
+             	<Question
+                 filteredQuestion={questionsFiltered[indexQuestion]}
+                 setIndexQuestion={setIndexQuestion}
+                 indexQuestion={indexQuestion}
+                 questionsFiltered={questionsFiltered}
+                 setActiveQuiz={setActiveQuiz}
+             />
+            ) : (
+                <>
+                <div className='flex flex-col gap-5'>
+                    <h1 className='text-3xl text-teal-900 text-center font-bold'>
+                        {category}
+                    </h1>
 
-            <Question filteredQuestion={questionsFiltered[indexQuestion]} 
-            setIndexQuestion={setIndexQuestion}
-            indexQuestion={indexQuestion}
-            questionsFiltered={questionsFiltered}/>
+                    <div className='flex justify-center items-center'>
+                        <img
+                            src= {imgCategory}
+                            alt={category}
+                            className='w-72'
+                        />
+                    </div>
+                </div>
+
+                <button
+                    className='text-white bg-gray-900 py-2 rounded-lg font-bold px-5 transition-all hover:bg-yellow-500 hover:text-gray-900'
+                    onClick={() => setActiveQuiz(true)}
+                >
+                    Iniciar Quiz
+                </button>
+            </>
+            )}
+
         </div>
     )
 }
