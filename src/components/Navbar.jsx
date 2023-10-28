0691ba52dd2e9b860650189172eeb98aaf5077c7
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2023-10-28 20:26:35 -0300

    haciendo responsil pag principal//pc escritorio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom"
 export const NavBar = () => {
     
   return (
-    <header className=" py-5 flex justify-center">
+    <header className=" py-0 sm:py-5 flex justify-center">
         <Link to = '/'>
-            <h1 className="text-white text-2x1 mt-24 font-titulo text-6xl hover:scale-110 duration-200">Play Ask</h1>
+            <h1 className="text-white text-2x1 mt-4 sm:mt-20  font-titulo text-6xl hover:scale-110 duration-200">Play Ask</h1>
         </Link>
     </header>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { CategoryList } from "../components/CategoryList";
 
 export const HomePage = () => {
-    return <div className="glass container max-w-4xl mx-auto flex justify-center items-center flex-wrap p-5 mt-5">
+    return <div className="glass container max-w-4xl mx-auto flex justify-center items-center flex-wrap p-0 mt-8 sm:p-5 sm:mt-20  ">
         <CategoryList/> </div>;
 };
